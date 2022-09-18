b142ca7a3ca61d3b515cbeff314fc2d85fe39729
Nam <96808045+RickAndMhorti@users.noreply.github.com>
2022-09-19 01:02:23 +0200

    Updated Play command
    yeah

--- commands/play.js
+++ commands/play.js
@@ -11,10 +11,35 @@ module.exports = {
         if(!voiceChannel) return message.channel.send("You gotta be in a voice channel first bruv");
         
         const permissions = voiceChannel.permissionsFor(message.client.user);
+        
         if(!permissions.has('CONNECT')) return message.channel.send("You're not supposed to do that");
         if(!permissions.has('SPEAK')) return message.channel.send("You're not supposed to do that");
         if(!args.length) return message.channel.send("Tell me what to play lmao");
-        
+
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        if(validURL(args[0])){
+            message.channel.send('ok buddy');
+            const connection = await voiceChannel.join();
+            const stream = ytdl(args[0], {filter: 'audioonly', type: 'opus'});
+
+            connection.play (stream, {seek:0, volume:0.3})
+            .on('finish', () =>{
+                voiceChannel.leave();
+                message.channel.send('Bye bye russia. bye bye');
+            });
+
+            await message.reply('I am now playing your link');
+
+            return;
+        }
         
         const connection = await voiceChannel.join();
 
@@ -28,13 +53,14 @@ module.exports = {
 
         if(video){
 
-            const stream = ytdl(video.url, {filter: 'audioonly'});
-            connection.play(stream, {seek:0, volume:0.6})
+            const stream = ytdl(video.url, {filter: 'audioonly', type: 'opus'});
+            connection.play (stream, {seek:0, volume:0.3})
             .on('finish', () =>{
                 voiceChannel.leave();
+                message.channel.send('Bye bye russia. bye bye');
             });
 
-            await message.reply(':nerd: Now Playing **' + video.title + '**');
+            await message.reply('I am now playing :nerd: ***' + video.title + '*** :nerd:');
         }
         else{message.channel.send("Video doesn't exist");}
     }
